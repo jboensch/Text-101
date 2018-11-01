3e6f91a59b8d65516f36cde3974175a8b730088b
Joshua Boensch <josh.boensch@gmail.com>
2018-11-01 00:01:31 -0400

    Section 3 - Lecture 24

--- Text_101/Assets/State.cs
+++ Text_101/Assets/State.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class State : ScriptableObject {
 
     //The [TextArea(14,10)] modifies the properties of the input text box in Unity, so when we are in the UI, it is easier to see the text there
-    [TextArea(14,10)] [SerializeField] string storyText;
+    [TextArea(14, 10)] [SerializeField] string storyText;
 
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 }
