3dbcf9c6b9cdd23e7493199c0e5c00565edd1467
Joshua Boensch <josh.boensch@gmail.com>
2018-10-31 23:31:05 -0400

    Section 3 - Lecture 23

---
+++ Text_101/Assets/State.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    //The [TextArea(14,10)] modifies the properties of the input text box in Unity, so when we are in the UI, it is easier to see the text there
+    [TextArea(14,10)] [SerializeField] string storyText;
+
+}
